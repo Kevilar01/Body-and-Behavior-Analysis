17ff42368658f70b301fd975032ac2353c30e6e0
Fisher Frederick <kevilar2024@gmail.com>
2025-07-29 16:44:44 +0100

    Your commit message

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import sitemap from "vite-plugin-sitemap";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -11,8 +11,10 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    sitemap({
+      hostname: "https://www.bodyandbehavioranalysis.com",
+    }),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
